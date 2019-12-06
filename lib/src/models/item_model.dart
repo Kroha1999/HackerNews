082403c2d874fcd32399a64efb2725c2e52e933d
Bodka <kroxmaluker@gmail.com>
2019-12-06 15:53:05 +0200

    Sqflite db added

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ItemModel {
   final int id;
   final bool deleted;
@@ -27,4 +29,41 @@ class ItemModel {
         score = parsedJson['score'],
         title = parsedJson['title'],
         descendants = parsedJson['descendants'];
+
+  ItemModel.fromDb(Map<String, dynamic> parsedJson)
+      : id = parsedJson['id'],
+        // Bool -> 0 = false, 1 = true
+        deleted = parsedJson['deleted'] == 1,
+        type = parsedJson['type'],
+        by = parsedJson['by'],
+        time = parsedJson['time'],
+        text = parsedJson['text'],
+        dead = parsedJson['dead'] == 1,
+        parent = parsedJson['parent'],
+        // Kids = decodes from BLOB to list<Int>
+        kids = json.decode(parsedJson['kids']),
+        url = parsedJson['url'],
+        score = parsedJson['score'],
+        title = parsedJson['title'],
+        descendants = parsedJson['descendants'];
+
+  Map<String, dynamic> toMapDb() {
+    return <String, dynamic>{
+      "id": id,
+      // Transform to SQLlite standarts bool = 1/0
+      "deleted": deleted ? 1 : 0,
+      "type": type,
+      "by": by,
+      "time": time,
+      "text": text,
+      "dead": dead ? 1 : 0,
+      "parent": parent,
+      // Transform to SQLlite standarts kids = String 
+      "kids": json.encode(kids),
+      "url": url,
+      "score": score,
+      "title": title,
+      "descendants": descendants,
+    };
+  }
 }
---
+++ lib/src/resources/news_db_provider.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+import 'dart:async';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+
+import '../models/item_model.dart';
+
+class NewsDbProvider {
+  Database db;
+
+  init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, "items.db");
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        // bool are represented as INTEGER
+        // list<int> is represented as BLOB -> decode must be done
+        newDb.execute("""
+          CREATE TABLE Items
+            (
+              id INTEGET PRIMARY KEY,
+              deleted INTEGER,
+              type TEXT,
+              by TEXT,
+              time INTEGER,
+              text TEXT,
+              dead INTEGER, 
+              parent INTEGET,
+              kids BLOB,
+              url TEXT,
+              score INTEGER,
+              title TEXT,
+              descendants INTEGER
+            )
+        """);
+      },
+    );
+  }
+
+  Future<ItemModel> fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    if (maps.length > 0) {
+      return ItemModel.fromDb(maps[0]);
+    }
+
+    return null;
+  }
+
+  addItem(ItemModel item) {
+    return db.insert("Items", item.toMapDb());
+  }
+}
